a1bb1e80879ec77b538b34597d6d4347ca469590
Kass <noam.kass@xmcyber.com>
2020-08-10 16:04:55 +0300

    Second Attempt

--- test1.ts
+++ test1.ts
@@ -10,6 +10,7 @@ export = class Test {
         yield this.init();
 
         const endTime = new Date();
+        const someChange = true;
         this.console.info(`Run completion time: ${endTime}. Duration: ${(endTime.getTime() - runStartTime.getTime()) / 1000} seconds`);
     }
 
